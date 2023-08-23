66f397b3ea1bbb9ffcb690d65438a6253dc5f7e8
Snehal Patil <141567039+snehal112211@users.noreply.github.com>
2023-08-23 23:29:10 +0530

    Add files via upload

---
+++ 5-BasicSyntax.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the value of the a");
+    scanf("%d",&a);
+    return 0;
+}
+
+// every line of c programming is terminated by the ;
+// token- means the small parts by whioch c program is form
+
+// do not use keywords to make variable
+// identifier means name of the function or variable .
+// it just our name use for our identification.
+
+// imp
+// our programme is form by keyword, identifier, string literals constant and symbols
+
+// there are 32 reserved keyword in the c program
+
+// white space in between the program does not affect its compilation.
